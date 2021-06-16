1854d30801193368299e205264ea8b3e2f1b931e
Rangga Mauliza <ranggamauliza@gmail.com>
2021-06-16 10:31:02 +0700

    setup initial state

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,10 @@ import { createStore, StoreOptions } from 'vuex'
 import { RootState } from './types'
 
 const store: StoreOptions<RootState> = {
-  isAppReady: false,
+  state: {
+    toastGeneral: false,
+    isAppReady: false,
+  },
 }
 
 export default createStore<RootState>(store)
--- src/store/types.ts
+++ src/store/types.ts
@@ -9,3 +9,41 @@ export interface UserState {
   email: string
   image: string
 }
+
+export interface VaultState {
+  id: string
+  name: string
+  totalProject: number
+  totalTask: number
+  totalUser: number
+  users: Array<UserState>
+  owner: UserState
+}
+
+export interface ProjectState {
+  id: string
+  name: string
+  taskCount: number
+}
+
+export interface TodoState {
+  id: string
+  assignUser: Partial<UserState>
+  completedDate: Date | null
+  createdAt: Date
+  isCompleted: boolean
+  label: string
+  name: string
+  parent: string | null
+  project: ProjectState
+  stage: number
+}
+
+export interface TrackState {
+  id: string
+  duration: number
+  end: Date | null
+  start: Date | null
+  todo: TodoState
+  project: ProjectState
+}
