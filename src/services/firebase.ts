f1951b358c7ffc34734c8e78b7a21d37dc286ebe
Rangga Mauliza <ranggamauliza@gmail.com>
2021-06-21 12:40:27 +0700

    init commit

--- src/services/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore, useFirestoreEmulator } from 'firebase/firestore'
-
-initializeApp({
-  // Firebase config settings here...
-})
-
-export const db = getFirestore()
-useFirestoreEmulator(db, 'localhost', 8080)
