01f81fd977c2a7c41543f1ad3866947e11053e50
Rangga Mauliza <ranggamauliza@gmail.com>
2021-06-21 12:30:58 +0700

    init test

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,13 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, useFirestoreEmulator } from 'firebase/firestore'
 
 initializeApp({
-  apiKey: 'AIzaSyD6RJx5vO_H15eWy7BIPoszYH7UOKN0_sg',
-  authDomain: 'rangkoding-todomer.firebaseapp.com',
-  projectId: 'rangkoding-todomer',
-  storageBucket: 'rangkoding-todomer.appspot.com',
-  messagingSenderId: '688740033613',
-  appId: '1:688740033613:web:460060c1ec92bff713a804',
-  // measurementId: 'G-J4G5WDZYCN',
+  // Firebase config settings here...
 })
 
 export const db = getFirestore()
